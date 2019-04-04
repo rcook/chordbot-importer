85943e2cdf8fb77637c1b503fa0ab3d1d44f54c4
Richard Cook <rcook@rcook.org>
2019-04-04 07:57:37 -0700

    Deserialize entire file format

--- src/Main.hs
+++ src/Main.hs
@@ -7,9 +7,10 @@ import qualified Data.ByteString.Lazy as ByteString
 import           Data.Monoid
 
 data Song = Song
-    { name :: String
+    { songName :: String -- TODO: Rename to "name"
     , fileType :: String
     , tempo :: Int
+    , sections :: [Section]
     , editMode :: Int
     } deriving Show
 
@@ -18,7 +19,58 @@ instance FromJSON Song where
         <$> o .: "songName"
         <*> o .: "fileType"
         <*> o .: "tempo"
+        <*> o .: "sections"
         <*> o .: "editMode"
+
+data Section = Section
+    { style :: Style
+    , chords :: [Chord]
+    , sectionName :: String -- TODO: Rename to "name"
+    } deriving Show
+
+instance FromJSON Section where
+    parseJSON = withObject "section" $ \o -> Section
+        <$> o .: "style"
+        <*> o .: "chords"
+        <*> o .: "name"
+
+data Style = Style
+    { tracks :: [Track]
+    , preset :: Int
+    , reverb :: Int
+    , chorus :: Int
+    } deriving Show
+
+instance FromJSON Style where
+    parseJSON = withObject "style" $ \o -> Style
+        <$> o .: "tracks"
+        <*> o .: "preset"
+        <*> o .: "reverb"
+        <*> o .: "chorus"
+
+data Track = Track
+    { volume :: Int
+    , trackId :: Int -- TODO: Rename to "id"
+    } deriving Show
+
+instance FromJSON Track where
+    parseJSON = withObject "track" $ \o -> Track
+        <$> o .: "volume"
+        <*> o .: "id"
+
+data Chord = Chord
+    { duration :: Int
+    , chordType :: String -- TODO: Rename to "type"
+    , root :: String
+    , bass :: Maybe String
+    } deriving Show
+
+instance FromJSON Chord where
+    parseJSON = withObject "chord" $ \o -> Chord
+        <$> o .: "duration"
+        <*> o .: "type"
+        <*> o .: "root"
+        <*> o .:? "bass"
 
 main :: IO ()
 main = do
