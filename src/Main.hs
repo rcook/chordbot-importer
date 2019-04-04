687fd6da66b71e7bd1000b57455d64beb5329e43
Richard Cook <rcook@rcook.org>
2019-04-04 07:45:54 -0700

    Parse basics

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
+
+import           Data.Aeson
+import qualified Data.ByteString.Lazy as ByteString
+import           Data.Monoid
+
+data Song = Song
+    { name :: String
+    , fileType :: String
+    , tempo :: Int
+    , editMode :: Int
+    } deriving Show
+
+instance FromJSON Song where
+    parseJSON = withObject "song" $ \o -> Song
+        <$> o .: "songName"
+        <*> o .: "fileType"
+        <*> o .: "tempo"
+        <*> o .: "editMode"
 
 main :: IO ()
 main = do
-    putStrLn "Hello world"
+    json <- ByteString.readFile "samples/minor-blues.json"
+    print json
+    let result = eitherDecode json :: Either String Song
+    print result
